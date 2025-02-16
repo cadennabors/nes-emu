bc8087a552019ca64a9754c41ff39782b9f09afa
aladvs <78510667+aladvs@users.noreply.github.com>
2025-02-16 17:18:11 -0600

    Create bus.rs

---
+++ src/bus.rs
@@ -0,0 +1,12 @@
+pub struct Bus {
+
+}
+
+impl Bus {
+    pub fn write(address : u16, data : u8) -> () {
+
+    }
+    pub fn read(address : u16, bReadOnly : Option<bool>) -> u8 {
+        return 0x00
+    }
+}
