271f3b6983064e7e425975905c23f9b7eb1705c2
aladvs <78510667+aladvs@users.noreply.github.com>
2025-02-17 17:32:15 -0600

    Create opcode_table.rs

---
+++ src/opcode_table.rs
@@ -0,0 +1,17 @@
+pub struct Opcode {
+    pub name : u8,
+    pub bytes : u8,
+    pub cycles : u8,
+    pub addressing_mode : AddressingMode
+}
+
+impl Opcode {
+    pub fn new(name : u8, bytes : u8, cycles : u8, addressing_mode : AddressingMode) -> Self {
+        Opcode {
+        name,
+        bytes,
+        cycles,
+        addressing_mode
+     }
+    }
+}
