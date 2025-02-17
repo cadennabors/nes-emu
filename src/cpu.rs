096a2905c290685278f726e0831abedf981d3661
aladvs <78510667+aladvs@users.noreply.github.com>
2025-02-17 00:24:54 -0600

    status fucntions

--- src/cpu.rs
+++ src/cpu.rs
@@ -8,6 +8,7 @@ pub struct CPU {
     bus : Bus,
 }
 
+
 impl CPU {
     pub fn new(bus : Bus) -> Self {
         CPU {
@@ -31,4 +32,63 @@ impl CPU {
     fn read(&mut self,addr : u16, _bReadOnly : Option<bool>) -> u8 {
         return self.bus.read(addr, _bReadOnly);
     }
+// ----------------STATUS----------------------
+fn set_carry_bit (&mut self) {
+    self.status |= 0b1000_0000
 }
+fn clear_carry_bit (&mut self) {
+    self.status |= !0b1000_0000
+}
+fn set_zero_bit (&mut self) {
+    self.status |= 0b0100_0000
+}
+fn clear_zero_bit (&mut self) {
+    self.status |= !0b0100_0000
+}
+fn set_disable_interrupts_bit (&mut self) {
+    self.status |= 0b0010_0000
+}
+fn clear_disable_interrupts_bit (&mut self) {
+    self.status |= !0b0010_0000
+}
+fn set_decimal_bit (&mut self) {
+    self.status |= 0b0001_0000
+}
+fn clear_decimal_bit (&mut self) {
+    self.status |= !0b0001_0000
+}
+fn set_break_bit (&mut self) {
+    self.status |= 0b0000_1000
+}
+fn clear_break_bit (&mut self) {
+    self.status |= !0b0000_1000
+}
+fn set_unused_bit (&mut self) {
+    self.status |= 0b0000_0100
+}
+fn clear_unused_bit (&mut self) {
+    self.status |= !0b0000_0100
+}
+fn set_overflow_bit (&mut self) {
+    self.status |= 0b0000_0010
+}
+fn clear_overflow_bit (&mut self) {
+    self.status |= !0b0000_0010
+}
+fn set_negative_bit (&mut self) {
+    self.status |= 0b0000_0001
+}
+fn clear_negative_bit (&mut self) {
+    self.status |= !0b0000_0001
+}
+}
+
+// C - Carry Bit 0b1000_0000
+// Z - Zero 0b0100_0000
+// I - Disable Interrupts 0b0010_0000
+// D - Decimal Mode (unused) 0b0001_0000
+// B - Break 0b0000_1000
+// U - Unused 0b0000_0100
+// V - Overflow 0b0000_0010
+// N - Negative 0b0000_0001
+
