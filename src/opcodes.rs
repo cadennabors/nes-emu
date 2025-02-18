96cec7f6551bf896b8e2c2d443712ee68d905bda
aladvs <78510667+aladvs@users.noreply.github.com>
2025-02-18 15:11:26 -0600

    Update opcodes.rs

--- src/opcodes.rs
+++ src/opcodes.rs
@@ -76,3 +76,82 @@ pub const TAY : u8 = 0xA8;
 pub const TYA : u8 = 0x98;
 
 // ARITHMETIC
+/* ADC adds the carry flag and a memory value to the accumulator. 
+The carry flag is then set to the carry value coming out of bit 7,
+allowing values larger than 1 byte to be added together by carrying 
+the 1 into the next byte's addition. This can also be thought of as
+unsigned overflow. It is common to clear carry with CLC before adding
+the first byte to ensure it is in a known state, avoiding an off-by-one
+error. The overflow flag indicates whether signed overflow or underflow
+occurred. This happens if both inputs are positive and the result is
+negative, or both are negative and the result is positive. */
+pub const ADC_IMM : u8 = 0x69;
+pub const ADC_ZP : u8 = 0x65;
+pub const ADC_ZP_X : u8 = 0x75;
+pub const ADC_ABS : u8 = 0x6D;
+pub const ADC_ABS_X : u8 = 0x7D;
+pub const ADC_ABS_Y : u8 = 0x79;
+pub const ADC_IND_X : u8 = 0x61;
+pub const ADC_IND_Y : u8 = 0x71;
+
+/* SBC subtracts a memory value and the bitwise NOT of carry from the
+accumulator. It does this by adding the bitwise NOT of the memory value 
+using ADC. This implementation detail explains the backward nature of 
+carry; SBC subtracts 1 more when carry is clear, not when it's set, and
+carry is cleared when it underflows and set otherwise. 
+As with ADC, carry allows the borrow from one subtraction to be carried
+into the next subtraction, allowing subtraction of values larger than 1
+byte. It is common to set carry with SEC before subtracting the first
+byte to ensure it is in a known state, avoiding an off-by-one error.
+
+Overflow works the same as with ADC, except with an inverted memory value. 
+Therefore, overflow or underflow occur if the result's sign is different 
+from A's and the same as the memory value's. */
+pub const SBC_IMM : u8 = 0xE9;
+pub const SBC_ZP : u8 = 0xE5;
+pub const SBC_ZP_X : u8 = 0xF5;
+pub const SBC_ABS : u8 = 0xED;
+pub const SBC_ABS_X : u8 = 0xFD;
+pub const SBC_ABS_Y : u8 = 0xF9;
+pub const SBC_IND_X : u8 = 0xE1;
+pub const SBC_IND_Y : u8 = 0xF1;
+
+/* INC adds 1 to a memory location. Notably, there is no version of this
+instruction for the accumulator; ADC or SBC must be used, instead.
+
+This is a read-modify-write instruction, meaning that it first writes
+the original value back to memory before the modified value. 
+This extra write can matter if targeting a hardware register.
+
+Note that increment does not affect carry nor overflow. */
+pub const INC_ZP : u8 = 0xE6;
+pub const INC_ZP_X : u8 = 0xF6;
+pub const INC_ABS : u8 = 0xEE;
+pub const INC_ABS_X : u8 = 0xFE;
+
+/* DEC subtracts 1 from a memory location. Notably, there is no version 
+of this instruction for the accumulator; ADC or SBC must be used, instead.
+
+This is a read-modify-write instruction, meaning that it first writes 
+the original value back to memory before the modified value. 
+This extra write can matter if targeting a hardware register.
+
+Note that decrement does not affect carry nor overflow. */
+pub const DEC_ZP : u8 = 0xC6;
+pub const DEC_ZP_X : u8 = 0xD6;
+pub const DEC_ABS : u8 = 0xCE;
+pub const DEC_ABS_X : u8 = 0xDE;
+
+// INX adds 1 to the X register. Note that it does not affect carry nor overflow.
+pub const INX : u8 = 0xE8;
+
+// DEX subtracts 1 from the X register. Note that it does not affect carry nor overflow.
+pub const DEX : u8 = 0xCA;
+
+// INY adds 1 to the Y register. Note that it does not affect carry nor overflow.
+pub const INY : u8 = 0xC8;
+
+// DEY subtracts 1 from the Y register. Note that it does not affect carry nor overflow.
+pub const DEY : u8 = 0x88;
+
+// SHIFT
