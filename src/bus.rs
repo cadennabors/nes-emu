373944213e4e3fe942ecca3268c136941cd47651
aladvs <78510667+aladvs@users.noreply.github.com>
2025-02-20 21:12:48 -0600

    rom for now

--- src/bus.rs
+++ src/bus.rs
@@ -1,13 +1,15 @@
 use crate::cpu::CPU;
 
 pub struct Bus {
-    ram : [u8; 64 * 1024]
+    ram : [u8; 64 * 1024],
+    rom : [u8; 64 * 1024],
 }
 
 impl Bus {
     pub fn new() -> Self {
         Bus {
-            ram: [0; 64 * 1024]
+            ram: [0; 64 * 1024],
+            rom: [0; 64 * 1024],
         }
     }
     
@@ -15,7 +17,10 @@ impl Bus {
         self.ram[addr as usize] = data;
     
     }
-    pub fn read(&mut self, addr : u16, _bReadOnly : Option<bool>) -> u8 {
+    pub fn read(&mut self, addr : u16, rom : Option<bool>) -> u8 {
+        if let Some(true) = rom {
+            return self.rom[addr as usize];
+        }
         return self.ram[addr as usize];
     }
 }
